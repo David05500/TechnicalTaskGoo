6e0a8e9c5b1118d0c7d40a1178f24986fe88d160
David Sarvasidze <sharvashidze.david@gmail.com>
2020-10-27 11:45:59 +0000

    added IOS like navigation

--- App.js
+++ App.js
@@ -1,14 +1,42 @@
 import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
+import { createStackNavigator, TransitionPresets, CardStyleInterpolators } from '@react-navigation/stack';
 import HomeScreen from './screens/HomeScreen';
+import DetailsScreen from './screens/DetailsScreen';
 const Stack = createStackNavigator();
 
 const App = () => (
   <NavigationContainer>
-      <Stack.Navigator>
-          <Stack.Screen name="Home" component={HomeScreen} />
+      <Stack.Navigator
+        screenOptions={{
+          gestureEnabled: true,
+          gestureDirection: "horizontal",
+          ...TransitionPresets.SlideFromRightIOS,
+          // cardStyleInterpolator: CardStyleInterpolators.forHorizontalIOS,
+          // transitionSpecs: {
+          //   open: config,
+          //   close: config
+          // }
+        }}
+        headerMode='float'
+      >
+          <Stack.Screen name="Home" options={{ title: 'Overview' }}>
+            {props => <HomeScreen {...props} extraData='Proppy' />}
+          </Stack.Screen>
+          <Stack.Screen name="Details" component={DetailsScreen} />
       </Stack.Navigator>
     </NavigationContainer>
 );
+
+const config = {
+  animation: 'spring',
+  config: {
+    stiffness: 1000,
+    damping: 50,
+    mass: 3,
+    overshootClamping: false,
+    restDisplacementThreshold: 0.01,
+    restSpeedThreshold: 0.01,
+  },
+};
 export default App;
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,15 +1,20 @@
 import React from 'react';
-import { SafeAreaView, View, Text } from 'react-native';
+import { SafeAreaView, View, Text, Button } from 'react-native';
 import tailwind from 'tailwind-rn';
 
-const HomeScreen = () => {
+const HomeScreen = ({navigation, ...props}) => {
+    
     return (
         <SafeAreaView style={tailwind('h-full bg-gray-100')}>
             <View style={tailwind('pt-12 items-center')}>
                 <View style={tailwind('bg-blue-200 px-3 py-1 rounded-full')}>
                     <Text style={tailwind('text-blue-800 font-semibold')}>
-                        Hello David
+                        Hello David {props.extraData}
                     </Text>
+                    <Button
+                        title="Go to Details"
+                        onPress={() => navigation.navigate('Details')}
+                    />
                 </View>
             </View>
         </SafeAreaView>
