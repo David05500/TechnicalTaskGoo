815d4c1f71b53efc84c46b3a8741aecc3f842a6f
David Sarvasidze <sharvashidze.david@gmail.com>
2020-10-27 10:39:46 +0000

    added TailwindCss

--- App.js
+++ App.js
@@ -1,21 +1,16 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {SafeAreaView, View, Text} from 'react-native';
+import tailwind from 'tailwind-rn';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Hi</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const App = () => (
+	<SafeAreaView style={tailwind('h-full bg-gray-100')}>
+		<View style={tailwind('pt-12 items-center')}>
+			<View style={tailwind('bg-blue-200 px-3 py-1 rounded-full')}>
+				<Text style={tailwind('text-blue-800 font-semibold')}>
+					Hello David
+				</Text>
+			</View>
+		</View>
+	</SafeAreaView>
+);
+export default App;
